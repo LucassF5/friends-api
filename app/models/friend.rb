396d8a7c9c1d336c1cdea80f6c360d2e26701f01
L F <franco100lucas@gmail.com>
2024-07-18 19:50:33 -0300

    feat: Implement fields validation in Friend model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,6 @@
 class Friend < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :phone, presence: true, length: { minimum: 9 }, numericality: { only_integer: true }
+  validates :email, presence: true, length: { minimum: 5 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :location, presence: true, length: { minimum: 2 }
 end
