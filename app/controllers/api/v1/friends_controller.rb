1635fe55ca311eae8d7542b303077b1a60347247
L F <franco100lucas@gmail.com>
2024-07-18 12:47:56 -0300

    feat: Add show, update, and delete endpoints for friends

--- app/controllers/api/v1/friends_controller.rb
+++ app/controllers/api/v1/friends_controller.rb
@@ -10,7 +10,40 @@ class Api::V1::FriendsController < ApplicationController
     if friend.save
       render json: {status: "SUCCESS", message: "Friend created successfully", data: friend}, status: :created
     else
-      render json: friend.errors, status: :unprocessable_entity
+      render json: {status: "ERROR", message: "Friend could not be created", data: friend.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    id = params[:id]
+    friend = Friend.find(id)
+
+    if friend
+      render json: {status: "SUCCESS",  data: friend}, status: :ok
+    else
+      render json: {status: "ERROR", message: "Friend not found"}, status: :bad_request
+    end
+  end
+
+  def update
+    id = params[:id]
+    friend = Friend.find(id)
+
+    if friend.update(friend_params)
+      render json: {status: "SUCCESS", message: "Friend updated successfully", data: friend}, status: :ok
+    else
+      render json: {status: "ERROR", message: "Friend could not be updated", data: friend.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    id = params[:id]
+    friend = Friend.find(id)
+
+    if friend.destroy
+      render json: {status: "SUCCESS", message: "Friend deleted successfully", data: friend}, status: :ok
+    else
+      render json: {status: "ERROR", message: "Friend could not be deleted", data: friend.errors}, status: :bad_request
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "friends", to: "friends#index"
       post "friends", to: "friends#create"
+      get "friends/:id", to: "friends#show"
+      patch "friends/:id", to: "friends#update"
+      delete "friends/:id", to: "friends#destroy"
     end
   end
 end
